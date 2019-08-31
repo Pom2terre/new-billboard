fb6166f99b9bdd0f81df9e5922c0d1892a554fc4
Glitch (southern-tax) <none>
2019-08-31 15:09:17 +0000

    📍🎢 Updated with Glitch

---
+++ auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const createJWT = options => {
+  return jwt.sign({}, process.env.JWT_SECRET, {
+    expiresIn: options.maxAge || 3600
+  });
+}
+
+const verifyJWT = token => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if(err || !decodedToken) {
+        return reject(err);
+      }
+      resolve(decodedToken);
+    });
+  });
+}
+
+module.exports = { createJWT, verifyJWT };
--- server.js
+++ server.js
@@ -1,20 +1,102 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const Airtable = require('airtable');
 
-// http://expressjs.com/en/starter/static-files.html
+const fs = require('fs');
+const { createJWT, verifyJWT } = require('./auth');
+const cookieParser = require('cookie-parser');
+
+
+Airtable.configure({
+    apiKey: process.env.AIRTABLE_API_KEY
+})
+
+const base = require('airtable').base(process.env.AIRTABLE_BASE_NAME)
+const table = base(process.env.AIRTABLE_TABLE_NAME)
+
+
+app.use(express.json());
 app.use(express.static('public'));
+app.use(cookieParser());
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+const initializedFile = './.data/initialized';
+
+app.get('/admin/reset', (req, res) => {
+  try {
+    if(fs.existsSync(initializedFile)) {
+      verifyJWT(req.cookies.token)
+      .then(decodeToken => {
+        fs.unlink(initializedFile, err => {
+          if(err) {
+            console.log('Error removing the file');
+            res.status(500).end();
+            return
+          }
+          res.send('Session ended');
+        })
+      }).catch(err => {
+        res.status(400).json({message: "Invalid authorization token provided."})
+      })
+        
+    } else {
+      res.status(500).json({message: "No session started."})
+    }
+  }
+  catch(err) {
+    console.log(err);
+  }
+})
+
+
+app.get('/admin', (req, res) => {
+  if(fs.existsSync(initializedFile)) {
+    //admin auth already initialized
+    verifyJWT(req.cookies.token)
+      .then(decodeToken => {
+      res.sendFile(__dirname + '/views/admin.html')
+    }).catch(err => {
+      res.status(400).json({message: "Invalid auth token provided."})
+    })
+  } else {
+    //admin auth not initialized
+    const token = createJWT({
+      maxAge: 60 * 24 * 365  // 1 year
+    });
+    
+    fs.closeSync(fs.openSync('./.data/initialized', 'w'));
+    
+    res.cookie('token', token, { httpOnly: true, secure: true });
+    res.sendFile(__dirname + '/views/admin.html');
+  }
 });
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/form', (req, res) => {
+  const name = req.body.name
+  const email = req.body.email
+  const date = (new Date()).toISOString()
+
+  table.create({
+    "Name": name,
+    "Email": email,
+    "Date": date
+  }, (err, record) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    console.log(record.getId())
+  })
+
+  res.end()
+})
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
